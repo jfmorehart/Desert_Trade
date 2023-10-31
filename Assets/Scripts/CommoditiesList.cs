c51e58c4ccf743039261dd868699d749a267735f
bellinimoon <mg6839@nyu.edu>
2023-10-31 01:15:32 -0400

    Attempt Merge

---
+++ Assets/Scripts/CommoditiesList.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommoditiesList : MonoBehaviour
+{
+    enum Commodities
+    {
+        Jewelry,
+        Daggers,
+        Pots,
+        Dates,
+        Water,
+        Cotton,
+        Gold,
+        Copper,
+        Oil,
+        Myrrh,
+        Silk,
+        Textiles
+    }
+}
---
+++ Assets/Scripts/Towns/Town.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Town : MonoBehaviour
+{
+    public Dictionary<string, int[]> commodities = new Dictionary<string, int[]>();
+}
